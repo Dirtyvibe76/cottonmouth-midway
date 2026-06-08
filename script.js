02516f47aa32e07fd9e2bec723377ec9fa0b31f5
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-06-07 20:22:59 -0400

    Fix missing enter button script

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver(entries => {
-
-entries.forEach(entry => {
-
-if(entry.isIntersecting){
-
-entry.target.animate([
-{opacity:0,transform:"translateY(50px)"},
-{opacity:1,transform:"translateY(0px)"}
-],{
-duration:1000,
-fill:"forwards"
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        [
+          { opacity: 0, transform: "translateY(50px)" },
+          { opacity: 1, transform: "translateY(0px)" }
+        ],
+        {
+          duration: 1000,
+          fill: "forwards"
+        }
+      );
+    }
+  });
 });
 
-}
-
-});
-
-});
-
-sections.forEach(section=>{
-observer.observe(section);
+sections.forEach(section => {
+  observer.observe(section);
 });
