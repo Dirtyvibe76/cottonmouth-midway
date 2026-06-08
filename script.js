3e629117f05397ca4b85a9a1c5fa50e64987876d
Dirtyvibe76 <aronsilvernailj@gmail.com>
2026-06-07 21:58:07 -0400

    Fix midway reveal styling

--- script.js
+++ script.js
@@ -10,42 +10,25 @@ ambienceAudio.loop = true;
 ambienceAudio.volume = 0.25;
 
 enterButton.addEventListener("click", () => {
-
     gateAudio.currentTime = 0;
-    gateAudio.play();
+    gateAudio.play().catch(() => {});
 
     landing.classList.add("fade-out");
 
     setTimeout(() => {
+        gateFlash.textContent = "OPEN THE GATES!";
+        gateFlash.classList.add("show");
+    }, 600);
 
+    setTimeout(() => {
         landing.style.display = "none";
-
-        gateFlash.style.display = "flex";
-        gateFlash.innerHTML = "OPEN THE GATES!";
-        gateFlash.classList.add("active");
-
-    }, 1000);
+        gateFlash.textContent = "WELCOME TO THE MIDWAY";
+    }, 1700);
 
     setTimeout(() => {
-
-        gateFlash.innerHTML = "WELCOME TO THE MIDWAY";
-
-    }, 2000);
-
-    setTimeout(() => {
-
-        gateFlash.classList.remove("active");
-        gateFlash.style.display = "none";
-
+        gateFlash.classList.remove("show");
         midway.classList.remove("hidden");
-
-        ambienceAudio.play();
-
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-
-    }, 3500);
-
+        ambienceAudio.play().catch(() => {});
+        window.scrollTo(0, 0);
+    }, 3200);
 });
